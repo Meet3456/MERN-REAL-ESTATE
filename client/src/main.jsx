a9da190dd11c89147d6151aa520147e214db0a55
Meet3456 <meetvasa09@gmail.com>
2024-04-19 14:01:12 +0530

    added redux persist so  that current user is logged in , even on page/app refresh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store.js";
 
 // Provide the Redux Store to React
 ReactDOM.createRoot(document.getElementById("root")).render(
   //  Importing the Redux store we just created, put a <Provider> around your <App>, and pass the store as a prop:
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,31 @@
 // configuring redux toolkit so that user data can be used globally anywhere in the application:
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// combineReducers is used to combine multiple reducers into a single reducer object. In this case, we are combining the userReducer into a single rootReducer object.
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// purpose of Redux Persist is to persist the Redux store between sessions, ensuring that the application's state is saved and can be reloaded even after the app is restarted or the page is refreshed.
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// persistReducer is used to create a new reducer with persistence capabilities. It takes the persistConfig and the rootReducer as arguments.
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
-  // disabling the serializableCheck middleware to avoid the error of serializing the user object: 
+  reducer: persistedReducer,
+  // disabling the serializableCheck middleware to avoid the error of serializing the user object:
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
