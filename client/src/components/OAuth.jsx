c37eb808bc830b2bd182f918413fc977a67d0a9e
Meet3456 <meetvasa09@gmail.com>
2024-04-26 01:09:29 +0530

    minor changes to oauth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,11 +2,11 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
   const dispatch = useDispatch();
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -29,6 +29,7 @@ export default function OAuth() {
 
       const data = await res.json();
       dispatch(signInSuccess(data));
+      navigate("/")
 
     } catch (error) {
       console.log("error", error);
