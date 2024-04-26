5d77124639285666c965b7054ce8437fc6dac8b1
Meet3456 <meetvasa09@gmail.com>
2024-04-26 10:38:17 +0530

    Complete image(profile picture) fuctionality added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyArTKtNCRfi6OgyMqMNq1rpHDtyT6099GE",
   authDomain: "mern-estate-3c26c.firebaseapp.com",
